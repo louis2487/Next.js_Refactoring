704ce2aae31b30ea6ca92ce1ec904b4f2752fa07
louis <louis36rain7@naver.com>
2025-05-27 17:24:53 +0900

    prisma Update

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from '../../../generated/prisma'
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  const users = await prisma.user.findMany();
+  return Response.json(users);
+}
