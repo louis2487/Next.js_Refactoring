e67e5edb9c091b708ec7a548dc661a721df8567b
louis <louis36rain7@naver.com>
2025-08-10 19:04:25 +0900

    privacy

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,13 +1,11 @@
 'use client';
+import '../globals.css';
 import Link from 'next/link';
-import { useDispatch, useSelector } from 'react-redux';
-import './globals.css';
-import { RootState } from './store/store';
-import { logout } from './store/userslice';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../store/store'; 
+import { logout } from '../store/userslice';
 
-
-
-export default function Home() {
+export default function Question() {
   const {userlogin, useremail}= useSelector((state: RootState) => state.myuser);
 
   const dispatch = useDispatch();
@@ -21,15 +19,15 @@ export default function Home() {
   };
 
   return (
-    <main className="home-main">
+    <main className="Question-main">
       <header className="home-header">
         <div className="header-title">SmartGauge</div>
         <nav>
           <ul className="header-nav">
-            <li><Link href="/" className="nav-link">홈</Link></li>
-            <li><Link href="/Info" className="nav-link">소개</Link></li>
-            <li><Link href="/R1" className="nav-link">점검</Link></li>
-            <li><Link href="/Question" className="nav-link">문의</Link></li>
+            <li><a href="/" className="nav-link">홈</a></li>
+            <li><a href="/Info" className="nav-link">소개</a></li>
+            <li><a href="/R1" className="nav-link">점검</a></li>
+            <li><a href="/Question" className="nav-link">문의</a></li>
           </ul>
         </nav>
         {userlogin ? <div className='home-butt2'><div className='user-wellcom'>환영해요 {useremail}님!</div> 
@@ -48,8 +46,8 @@ export default function Home() {
 
       <div className="home-hero">
         <h1 className="home-hero-title">
-          불편한 차량 소모품 관리<br/>
-          이제 바로바로 스마트하게 확인해요!
+          문의하고 싶은 내용이 있다면<br />
+          smartgauge@www.smartgauge.co.kr 로 메일 보내주세요!
         </h1>
       </div>
     </main>
